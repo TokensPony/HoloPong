eadcc2a38dfa786bb6a77802996fcf5f1e0af9ba
TokensPony <cmtherealmccoy@gmail.com>
2019-08-08 16:25:34 -0500

    first 3-sided camera build. Need to adjust positioning and brightness

--- Assets/BumperScript.cs
+++ Assets/BumperScript.cs
@@ -19,13 +19,14 @@ public class BumperScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!collided)
-        {
-            if (isBump1)
-                transform.Translate(0f, Input.GetAxis("Vertical") * speed * Time.deltaTime, Input.GetAxis("Horizontal") * speed * Time.deltaTime);
-            else
-                transform.Translate(0f, Input.GetAxis("Vertical2") * speed * Time.deltaTime, Input.GetAxis("Horizontal2") * speed * Time.deltaTime);
-        }
+		if (!collided) {
+			if (isBump1)
+				transform.Translate (0f, Input.GetAxis ("Vertical") * speed * Time.deltaTime, Input.GetAxis ("Horizontal") * speed * Time.deltaTime);
+			else
+				transform.Translate (0f, Input.GetAxis ("Vertical2") * speed * Time.deltaTime, Input.GetAxis ("Horizontal2") * speed * Time.deltaTime);
+		} else {
+			transform.Translate (0f, 0f, 0f);
+		}
 
         collided = false;
     }
